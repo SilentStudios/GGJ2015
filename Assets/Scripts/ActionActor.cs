85c72409ef6c399b942a0d31301f88830bb921e2
Román <roman.diaz.leon@gmail.com>
2015-01-24 04:50:47 +0000

    Changed ActionActor to abstract clas

--- Assets/Scripts/ActionActor.cs
+++ Assets/Scripts/ActionActor.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-interface ActionActor : MonoBehaviour {
+abstract class ActionActor : MonoBehaviour {
 
 }
--- Assets/Scripts/StealthActionActor.cs
+++ Assets/Scripts/StealthActionActor.cs
@@ -10,5 +10,6 @@ public class StealthActionActor : ActionActor {
 	
 	// Update is called once per frame
 	void Update () {
+
 	}
 }
