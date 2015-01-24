c171b7f538490e376e82349c51548a746d97c846
Román <roman.diaz.leon@gmail.com>
2015-01-24 02:49:04 +0000

    Created actions of actor

---
+++ Assets/Scripts/CryActionActor.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CryActionActor : MonoBehaviour, BaseActionActor {
+
+	public void executeAction() {
+		Debug.Log ("Ejecutando accion de llorar");
+	}
+}
---
+++ Assets/Scripts/DanceActionActor.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DanceActionActor : MonoBehaviour, BaseActionActor {
+
+	public void executeAction() {
+		Debug.Log ("Ejecutando accion de bailar");
+	}
+}
