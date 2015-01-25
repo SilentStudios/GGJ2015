c1481d8776b80e951ec405e295a2339555df2745
Armando <armandovmj@gmail.com>
2015-01-25 15:21:48 +0100

    Added main menu instructions

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -1,7 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class SceneManager : MonoBehaviour {
+
+    public Image background;
+    public Sprite[] images;
+
+    public Button exit;
+    public Button start;
+
+    int index = 0;
+
+    void Start()
+    {
+        index = 0;
+    }
+
+    public void NextImage()
+    {
+        exit.gameObject.SetActive(false);
+        
+        if (index < images.Length)
+        {
+            background.sprite = images[index];
+			index++;
+        }
+        else
+        {
+            StartGame();
+        }
+    }
 
     public void StartGame()
     {
